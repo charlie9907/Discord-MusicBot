0bf01b5f98e1936c761da8bfe8f11fb2c2dbd620
charlie9907 <worksbynotsa.fanmail@gmail.com>
2026-02-28 13:39:26 +1000

    Update configuration values for bot settings

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "1375629140341952552", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTQ1NzU5ODIwMTI0MTUzNDUwOQ.GnwLoL.9HGHGqRh8N_GVxk4dGxyzrhh1Xq3kb6y_dFh0o", //- Bot's Token
+	clientId: process.env.clientId || "1457598201241534509", //- ID of the bot
+	clientSecret: process.env.clientSecret || "yzeo0HaOZsZXDg9CT0q-K_K2kGss6Oe9", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			host: "lavalinkv4.serenetia.com", //- The host name or IP of the lavalink server.
+			port: 443, // The port that lavalink is listening to. This must be a number!
+			password: "https://dsc.gg/ajidevserver", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
